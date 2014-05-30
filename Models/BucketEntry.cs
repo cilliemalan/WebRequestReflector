f87e4ebf72722ae621528df54ea83c9092825e97
Cillie Malan <cilliemalan@gmail.com>
2014-05-30 13:51:46 +0200

    Modified BucketEntry

--- Models/BucketEntry.cs
+++ Models/BucketEntry.cs
@@ -9,11 +9,9 @@ namespace WebRequestReflector.Models
 {
     public class BucketEntry
     {
-        public string Key { get; set; }
         public HttpRequestHeaders Headers { get; set; }
         public HttpMethod Method { get; set; }
         public string Contents { get; set; }
         public DateTimeOffset DateAdded { get; set; }
-        public DateTimeOffset Expires { get; set; }
     }
 }
