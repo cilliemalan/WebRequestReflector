4e913bf57e46b085a97a6765e732428b00a4c29d
Cillie Malan <cilliemalan@gmail.com>
2014-07-02 12:42:54 +0200

    Added SelfHostTest logic

---
+++ WebRequestReflector.Tests/SelfHostTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+
+namespace WebRequestReflector.Tests
+{
+	[TestClass]
+	public class SelfHostTests
+	{
+		[TestMethod]
+		public async Task SelfHostTest()
+		{
+			HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:9967/");
+			config.HostNameComparisonMode = HostNameComparisonMode.Exact;
+			WebRequestReflectorSelfHost wrrsh = new WebRequestReflectorSelfHost(config);
+
+			Assert.IsNotNull(wrrsh);
+
+			await wrrsh.StartAsync();
+			await wrrsh.StopAsync();
+		}
+	}
+}
