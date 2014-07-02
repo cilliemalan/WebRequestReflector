ec7ed5877bb0ab96d375693b68e2f36ce609c0f3
Cillie Malan <cilliemalan@gmail.com>
2014-07-02 15:56:55 +0200

    Whoops. my bad.

---
+++ WebRequestReflector.SelfHost/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace WebRequestReflector
+{
+	class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			HttpConfiguration configuration = new HttpConfiguration();
+			WebApiConfig.Register(configuration);
+			app.UseWebApi(configuration);
+			app.UseCors(CorsOptions.AllowAll);
+		}
+	}
+}
---
+++ WebRequestReflector.SelfHost/WebRequestReflectorSelfHost.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRequestReflector
+{
+	public class WebRequestReflectorSelfHost : IDisposable
+	{
+		private IDisposable _application;
+		private StartOptions _startupOptions;
+
+		public WebRequestReflectorSelfHost()
+			:this("http://localhost:8080/")
+		{
+		}
+
+		public WebRequestReflectorSelfHost(string baseUrl)
+			:this(new StartOptions(baseUrl))
+		{
+		}
+
+		private WebRequestReflectorSelfHost(StartOptions startupOptions)
+		{
+			_startupOptions = startupOptions;
+		}
+
+		public bool Started { get { return _application != null; } }
+		public StartOptions StartOptions { get { return _startupOptions; } set { _startupOptions = value; } }
+
+		public void Start()
+		{
+			if (_application != null) throw new InvalidOperationException("The web application is already started");
+
+
+			var app = WebApp.Start<Startup>(_startupOptions);
+			_application = app;
+		}
+
+		public void Stop()
+		{
+			if (_application == null) throw new InvalidOperationException("The web application is not started");
+
+			var app = _application;
+			_application = null;
+			app.Dispose();
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+
+			}
+		}
+
+		~WebRequestReflectorSelfHost()
+		{
+			Dispose(false);
+		}
+	}
+}
