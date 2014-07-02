deda66a9baba75c348aa6ea54b290015260be16d
Cillie Malan <cilliemalan@gmail.com>
2014-07-02 15:15:03 +0200

    Added SelfHostPingTest

--- WebRequestReflector.Tests/SelfHostTests.cs
+++ WebRequestReflector.Tests/SelfHostTests.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +15,43 @@ namespace WebRequestReflector.Tests
 	[TestClass]
 	public class SelfHostTests
 	{
+		public const string HostUrl = "http://localhost:9967/";
+
 		[TestMethod]
 		public async Task SelfHostTest()
 		{
-			HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:9967/");
+			HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(HostUrl);
 			config.HostNameComparisonMode = HostNameComparisonMode.Exact;
 			WebRequestReflectorSelfHost wrrsh = new WebRequestReflectorSelfHost(config);
 
 			Assert.IsNotNull(wrrsh);
 
 			await wrrsh.StartAsync();
+			await wrrsh.StopAsync();
+		}
+		
+		[TestMethod]
+		public async Task SelfHostPingTest()
+		{
+			HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(HostUrl);
+			config.HostNameComparisonMode = HostNameComparisonMode.Exact;
+			WebRequestReflectorSelfHost wrrsh = new WebRequestReflectorSelfHost(config);
+
+			Assert.IsNotNull(wrrsh);
+
+			await wrrsh.StartAsync();
+
+			using(HttpClient client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+				var response = await client.PostAsync(HostUrl + "create", new StringContent(""));
+				Assert.IsNotNull(response);
+				Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+				Assert.IsNotNull(response.Content);
+				Assert.AreEqual("application/xml", response.Content.Headers.ContentType.MediaType);
+			}
+
 			await wrrsh.StopAsync();
 		}
 	}
